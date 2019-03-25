1a7212d8fd78a38bb8ac432bac8a216c656c0090
hydroper <HydroperHH@gmail.com>
2019-03-25 10:29:54 -0300

    >

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+const commandLineArgs = require('command-line-args')
+    , commandLineUsage = require('command-line-usage')
+
+class Command {
+    constructor() {
+        this.
+    }
+}
+
+module.exports = { Command, }
