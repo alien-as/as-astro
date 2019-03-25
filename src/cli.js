a392e84840bf549c5864428513e68cf1add2e930
hydroper <HydroperHH@gmail.com>
2019-03-25 11:51:28 -0300

    >

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,34 @@
+//! Abstraction over
+//! `command-line-args` and
+//! `command-line-usage`.
+
 const commandLineArgs = require('command-line-args')
     , commandLineUsage = require('command-line-usage')
 
 class Command {
     constructor() {
-        this.
+        this._options = []
+        this._helpSections = []
     }
 
     /// <chainable/>
     ///
-    help() {}
+    helpSections(...sections) {
+        for (let sec of sections)
+            this._helpSections.push(sec)
+    }
+
+    option(option) {
+        this._options.push(option)
+        return this
+    }
+
+    optionsSection() {
+        return {
+            header: 'Options',
+            optionList: this._options,
+        }
+    }
 }
 
 module.exports = { Command, }
