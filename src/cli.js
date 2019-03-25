09f3fba3baf10f6fbae142af42c91ef0937cee80
hydroper <HydroperHH@gmail.com>
2019-03-25 14:38:22 -0300

    >

--- src/cli.js
+++ src/cli.js
@@ -11,6 +11,7 @@ class Command {
         this._options = []
         this._usage = []
         this._subCommands = null
+        this._subCommandOperation = new Map
     }
 
     subCommand(command) {
@@ -22,6 +23,8 @@ class Command {
     /// <chainable/>
     ///
     usage(section) {
+        if (section.optionList)
+            section.hide = 'command'
         this._usage.push(section)
     }
 
@@ -37,17 +40,51 @@ class Command {
         return this
     }
 
-    parse(argv) {
+    /// Maps operations to matched sub-commands.
+    ///
+    /// A `'#!'`-key property is treated
+    /// specially. It'll be called as `f(name)`
+    /// if a given sub-command doesn't exist.
+    ///
+    subCommandOperation(o) {
+        for (let k in o)
+            this._subCommandOperations.set(k, o[k])
+    }
+
+    parse() {
         try {
-            ...
+            const args = commandLineArgs(this._options,
+                { stopAtFirstUnknown: true })
+            if (args._unknown) {
+                if (args.command && args.command[0] !== '-') {
+                    const f = o[args.command]
+                    if (f) {
+                        const cmd = this._subCommands.get(args.command)
+                        return cmd.parse(???)
+                    }
+                    else if (o['#!'])
+                        o['#!'](args._unknown, args)
+                    else {
+                        console.error(`Unknown sub-command: ${args.command}`)
+                        process.exit(1)
+                    }
+                } else {
+                    console.error(`Unknown option: ${args._unknown[0]}`)
+                    console.exit(1)
+                }
+            }
+            if (args._help) {
+                ...
+            }
+            return args
         } catch (e) {
             console.error(e.message)
             process.exit(1)
         }
     }
 
-    getUsage() {
-        ...
+    usageString() {
+        return commandLineUsage(this._usage)
     }
 }
 
