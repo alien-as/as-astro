ad28653ed05e3b3c38bb1473753427473eb03197
hydroper <hydroperhh@gmail.com>
2019-03-26 20:03:13 -0300

    >

--- src/astro-bin/console_format.js
+++ src/astro-bin/console_format.js
@@ -1,9 +1,9 @@
 const chalk = require('chalk')
-const successTermColor = chalk.rgb(0x1A, 0xFF, 0x30)
+const successTermColor = chalk.rgb(0x1A, 0x30, 0xFF)
 
 module.exports = {
     fmtSuccess(msg) {
-        return chalk.green(msg)
+        return chalk.blue(msg)
     },
 
     fmtSuccessTerm(s) {
