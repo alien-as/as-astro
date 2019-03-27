092d686470ce3a6b2af77b8a2b782a0dc493f87e
hydroper <hydroperhh@gmail.com>
2019-03-27 18:52:45 -0300

    >

--- src/astro-lib/storage.js
+++ src/astro-lib/storage.js
@@ -34,9 +34,14 @@ let astroStorage = {
         if (!this._defaultCompiler) {
             const item = localStorage.getItem('default_bc')
             if (!item) return null
+
             const {name, version: verRaw} = JSON.parse(item)
             const ver = semver.validRange(verRaw)
+
+            if (!ver) return null
+
             const compilers = this.compilers()
+
             for (let bc of compilers) {
                 if (bc.name === name
                  && semver.satisfies(bc.version, ver))
