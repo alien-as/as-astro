c6297cd80fc8bbc4b943fed0bfc199ad766d0e35
hydroper <HydroperHH@gmail.com>
2019-03-28 09:02:39 -0300

    >

--- src/astro-bin/compilers/exec.js
+++ src/astro-bin/compilers/exec.js
@@ -1,15 +1,26 @@
-//! Execute default compiler on given package.
+//! Get compiler execution model.
+//!
+//! ```
+//! interface CompilerExec {
+//!     build(): 
+//!     run(): 
+//!     doc(): 
+//! }
+//! ```
 
 const {Command} = require('@astro-bin/cli')
     , structure = require('@astro-bin/structure')
-    , clfmt = require('@astro-bin/console-format')
-const {astroStorage} = require('@astro-lib/storage')
-const fs = require('fs')
-    , path = require('path')
-const prompts = require('prompts')
-    , chalk = require('chalk')
+    , clfmt     = require('@astro-bin/console-format')
 
-const air = require('./air')
+const {astroStorage} = require('@astro-lib/storage')
+
+const fs   = require('fs')
+    , path = require('path')
+
+const prompts = require('prompts')
+    , chalk   = require('chalk')
+
+const air    = require('./air')
     , redtam = require('./redtam')
 
 module.exports = {
@@ -25,9 +36,13 @@ module.exports = {
         const bc = astroStorage.defaultCompiler()
         if (!bc) {
             clfmt.error('No compiler specified.\n'
-                + chalk ``)???
+                + chalk `{italic $ astro bc default <compiler>}`)
             process.exit(1)
         }
-        ...
+        if (bc.isLocal) {
+            ...
+        } else {
+            ...
+        }
     },
 }
