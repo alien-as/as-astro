fefc4aea150ec9117ceb321c552b151fd0c01176
hydroper <hydroperhh@gmail.com>
2019-03-27 22:17:09 -0300

    >

---
+++ src/astro-bin/compilation.js
@@ -0,0 +1,10 @@
+/// Compilation processes.
+///
+/// * Red Tamarin: `redbean`
+///              + `asc`
+///
+/// * Fleꭗ-based SDKs: `[a]mxmlc`
+///                  + `[a]compc`
+///
+/// * Custom: unimplemented
+
